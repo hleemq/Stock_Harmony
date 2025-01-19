0a44baa109a7b81d279454380aa391b61bd6753d
hleemq <m.achraf.laqsour@gmail.com>
2025-01-19 16:19:20 +0000

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,6 +2,6 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export const createClient = () =>
   createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_KEY,
   );
