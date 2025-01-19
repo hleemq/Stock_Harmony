63ca2ce50e604b741f54b74fcdc82f768f18dbc1
hleemq <m.achraf.laqsour@gmail.com>
2025-01-19 16:14:35 +0000

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,10 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
-import type { Database } from './types';
 
 export const createClient = () =>
   createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   );
-
-export const supabase = createClient<Database>(NEXT_PUBLIC_SUPABASE_UR, EXT_PUBLIC_SUPABASE_ANON_KEY);
